1f8ed7d6a936bdd4ac4bce2e112c756d9779ada6
Rgcavalheiro <rafael.g.cavalheiro@gmail.com>
2019-07-16 16:12:11 -0300

    step1

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Academia Brasileira de Omaha</title>
+    <link rel="shortcut icon" href="img/favicon.png" type="image/png">
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="A gente respira POT LIMIT OMAHA!" />
+    <meta property="og:description" content="Eleve seu nível de jogo." />
+    <meta property="og:image" content="http://impactusweb.com.br/abplo/img/default.png" />
+    <meta property="og:site_name" content="A gente respira POT LIMIT OMAHA!" />
+    <meta property="og:locale" content="pt_BR" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,600i,700,700i,800|Roboto+Slab:100,300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+  </head>
+  <body>
+    <div class="cor">
+    <div class="content">
+      <div class="social-limit">
+        <h1 class="socialtitle left">Curta a ABPLO:</h1>
+        <div class="social left">
+          <a href="https://www.facebook.com/abplo/" target="_blank"><div class="social-box fb"></div></a>
+          <a href="https://www.instagram.com/academia_brasileira_de_omaha/" target="_blank"><div class="social-box insta"></div></a>
+          <a href="https://www.youtube.com/channel/UCUh-zI896k8r6bwg9OXieVA" target="_blank"><div class="social-box yt"></div></a>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+    <header class="topo">
+      <div class="content">
+        
+<!--         <h1><b>Faça seu cadastro e tenha seu nome no topo da lista para nossa próxima seleção do ABPLO Team.</b></h1>
+        <p class="tagline"> O primeiro time online de Omaha do Brasil</p> -->
+      </div>
+    </header>
+    <main>
+      <div class="content">
+        <form class="formulario" action="https://www.abplo.com.br/teamobrigado/" method="post">
+ 	   <input class="form-control form-nome" name="nome" placeholder="Seu nome" type="text"   />   
+	   <input class="form-control form-email" name="email" placeholder="Seu email" type="text"   />   
+	   <input class="form-control form-celular" id="celular" name="celular" placeholder="Seu whatsapp" type="text"  data-mask="(00) 0-0000-0000" data-mask-selectonfocus="true"/>  
+           <input type="submit" value="Enviar cadastro" class="center" style="width: 51%; " >
+        </form>
+        <div class="clear"></div>
+      
+      </div>
+    </main>
+  </div>
+  <script type="text/javascript" async src="https://d335luupugsy2.cloudfront.net/js/loader-scripts/64685cab-76e1-4d55-adb6-e20054b0d0d4-loader.js" ></script>
+
+
+  <script
+  src="https://code.jquery.com/jquery-3.4.0.js"
+  integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo="
+  crossorigin="anonymous"></script>
+
+  <script src="jquery.mask.js"/>
+
+
+  <script type="text/javascript"> 
+
+
+
+$(document).ready(function(){
+
+  $('#celular').mask('(00) 0000-0000');
+  
+ 
+
+
+
+  </script>
+
+
+  </body>
+</html>
